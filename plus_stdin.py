fc811eb3fe4f4cd183e5ad719296dcb046a65402
shimonotuke1535 <simonomao1535@gmail.com>
2022-10-13 12:32:22 +0900

    oh tin tin

--- plus_stdin.py
+++ plus_stdin.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-x = 0.0
-for n in sys.stdin:
-        x += float(n)
-print(x)
+x = 0
+for line in sys.stdin:
+    try:
+        x += int(line)
+    except:
+        x += float(line)
+    print(x)
