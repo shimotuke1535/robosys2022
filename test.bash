b7d8013ac8e9db5b20c949e629a51530ad0fbadc
shimonotuke1535 <simonomao1535@gmail.com>
2022-10-27 12:49:21 +0900

    2022/10/27

--- test.bash
+++ test.bash
@@ -1,5 +1,27 @@
-#!/bin/bash
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: This Softwere Package allowed Redistribution and Use by GPL 3.0
+ng()
+{
+	echo NG at Line $1
+	res=1
+}
 
+res=0
+
+###I/O TEST###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+###STRINPUT TEST###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+###EMPTY INPUT TEST###
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+ 
+[ "$res" = 0 ] && echo OK
+exit $res
